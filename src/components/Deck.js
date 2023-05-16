0c4961192344cbb8a9d786ef76c2d3c7c143e29b
Joshua Bell <83473305+jjohnjbell@users.noreply.github.com>
2023-05-16 12:52:45 -0500

    commit on assesmentFork

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -5,22 +5,32 @@ import { useNavigate } from 'react-router-dom';
 
 
 
+
 export default function Deck(props) {
     const [cardTotal, setCardTotal] = useState('');
     const navigate = useNavigate();
+    const [bgImg, setBgImg] = useState('../images/blackBg.svg')
+
 
 
     return (
-        <div className='deckItem' >
+        <li className='deckItem' >
 
-            <div className='deckHeader'>
+            <div className='deckHeader' style={{ backgroundImage: `url(${bgImg})` }}>
+
+                <div className="deckHeaderColumn">
+                    <img className="deckHeaderIcon" src='./images/deckIcon.svg' />
+                    <h3 className="deckName">Name</h3>
+                </div>
+
+                <div className="deckDeleteIconContainer"> <img id="deleteGroupIcon" src='./images/deleteGroupIcon.svg' /> </div>
 
             </div>
 
             <div className='deckBody'>
 
             </div>
-        </div>
+        </li>
 
     )
 }
